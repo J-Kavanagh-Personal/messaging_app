d9227ad73388f10eed4b84e40bfe4b4f3131d23d
James Kavanagh <jkav0872@gmail.com>
2024-02-13 16:26:51 +0000

    refactoring

--- spec/jobs/send_message_job_spec.rb
+++ spec/jobs/send_message_job_spec.rb
@@ -1,19 +1,23 @@
 # frozen_string_literal: true
 
-require 'rspec'
+require 'rails_helper'
 
-RSpec.describe 'SendMessageJob' do
-  before do
-    # Do nothing
+RSpec.describe SendMessageJob, type: :job do
+  include ActiveJob::TestHelper
+
+  let(:message) { FactoryBot.create(:message) }
+  subject(:job) { described_class.perform_later(message) }
+
+
+  it 'queues the job' do
+    expect { job }
+      .to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(1)
   end
 
-  after do
-    # Do nothing
-  end
-
-  context 'when condition' do
-    it 'succeeds' do
-      pending 'Not implemented'
+  it 'broadcasts on the channel' do
+    perform_enqueued_jobs do
+      expect_any_instance_of(ActionCable::Server::Base).to receive(:broadcast)
+      job
     end
   end
 end
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -2,18 +2,26 @@
 
 require 'rails_helper'
 
-RSpec.describe 'UserMailer' do
-  before do
-    # Do nothing
-  end
+RSpec.describe UserMailer, type: :mailer do
+  describe "weekly_message_update_email" do
 
-  after do
-    # Do nothing
-  end
+    let(:total_messages) { 10 }
+    let(:total_since_been_gone) { 10 }
+    let(:user) { FactoryBot.build(:user) }
+    let(:params) { { user: user, total_messages: total_messages, total_since_been_gone:
+      total_since_been_gone }}
+    let(:mail) { UserMailer.with(params).weekly_message_update_email }
 
-  context 'when condition' do
-    it 'succeeds' do
-      pending 'Not implemented'
+    it "renders the headers" do
+      expect(mail.subject).to eq("Weekly message update")
+      expect(mail.to).to eq(["#{FactoryBot.build(:user).email}"])
+      expect(mail.from).to eq(['notifications@messaging_app.com'])
+    end
+
+    it "renders the body" do
+      expect(mail.body).to match("Hello here are you stats for last week. Total messages sent last week
+#{total_messages}
+Total messages since you last signed in at #{user.last_sign_in_at} was #{total_since_been_gone}")
     end
   end
 end
