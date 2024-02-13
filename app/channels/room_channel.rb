a9ee08e21d012f2b8694bd8771b4671e6d12f653
James Kavanagh <jkav0872@gmail.com>
2024-02-13 12:36:59 +0000

    refactoring

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -2,4 +2,8 @@ class RoomChannel < ApplicationCable::Channel
   def subscribed
     stream_from "RoomChannel"
   end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
 end
